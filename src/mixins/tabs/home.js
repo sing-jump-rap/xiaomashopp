6bb816d22e45e394b1f874093769cde915bafe9a
MaTeng <1749156618@qq.com>
2019-11-02 16:51:10 +0800

    分类完成，优化（封装）了常用方法和请求

---
+++ src/mixins/tabs/home.js
@@ -0,0 +1,51 @@
+import wepy from 'wepy'
+
+export default class Home extends wepy.mixin {
+    data = {
+        // 存放轮播图数据
+        swiperData:[],
+        // 存放分类数据
+        cateData:[]
+    }
+
+    config = {
+    }
+
+    methods = {
+
+    }
+
+    //获取轮播图数据
+    async getSwiperData() {
+        const {data:res} = await wepy.get('/home/swiperdata')
+        if (res.meta.status!=200) {
+            //失败提示处理
+            return wepy.baseToaset()
+        }
+        this.swiperData = res.message
+        this.$apply()  
+    }
+
+
+
+    //获取分类数据
+    async getCateData() {
+        const {data} = await wepy.get('/home/catitems')         
+        if (data.meta.status !== 200) {
+            return wepy.baseToaset()
+        }
+        this.cateData = data.message
+        this.$apply()
+    }
+
+
+
+    // 页面开始加载
+    onLoad() {
+        // 加载轮播图
+        this.getSwiperData()
+        // 加载分类
+        this.getCateData()
+        
+    }
+}
---
+++ src/util/index.js
@@ -0,0 +1,31 @@
+import wepy from 'wepy'
+const baseUrl = 'https://uinav.com/api/public/v1'
+
+
+// title 提示信息的文字  icon 提示信息的图标
+// 封装获取信息失败方法
+wepy.baseToaset = function (title='获取数据失败',icon='none') {
+    wepy.showToast({
+        title,
+        icon
+    })
+}
+
+
+// 封装get请求
+wepy.get = function (path,data={}) {
+    return wepy.request({
+        url:baseUrl + path,
+        data
+    })
+}
+
+
+// 封装post请求
+wepy.post = function (path,data={}) {
+    return wepy.request({
+        url:baseUrl + path,
+        data,
+        method:'post'
+    })
+}
